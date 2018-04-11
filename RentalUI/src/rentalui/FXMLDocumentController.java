eef43b20111bcc5cf329266355655bf48eebba2a
MaikoOB6622 <37833622+MaikoOB6622@users.noreply.github.com>
2018-04-11 14:23:25 +0300

    Add files via upload

---
+++ RentalUI/src/rentalui/FXMLDocumentController.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rentalui;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+
+/**
+ *
+ * @author Mike
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private ImageView b_Payment;
+    @FXML
+    private ImageView b_House;
+    @FXML
+    private ImageView b_monthly;
+    @FXML
+    private AnchorPane h_PaymentDetails;
+    @FXML
+    private AnchorPane h_HouseDetails;
+    @FXML
+    private AnchorPane h_MonthlyExpenditure;
+    
+    @FXML
+    private void handleButtonAction(MouseEvent event) {
+        if (event.getTarget() == b_House) {
+            h_HouseDetails.setVisible(true);
+            h_PaymentDetails.setVisible(false);
+            h_MonthlyExpenditure.setVisible(false);
+        } else 
+        if (event.getTarget() == b_Payment){
+            h_PaymentDetails.setVisible(true);
+            h_HouseDetails.setVisible(false);
+            h_MonthlyExpenditure.setVisible(false);
+        }else
+        if (event.getTarget() == b_monthly){
+            h_MonthlyExpenditure.setVisible(true);
+            h_HouseDetails.setVisible(false);
+            h_PaymentDetails.setVisible(false);
+        }
+    }
+    
+    ObservableList<String>HouseNumber = FXCollections.observableArrayList("A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "A11", "A12");
+    ObservableList<String>MonthPaid = FXCollections.observableArrayList("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+    
+    @FXML
+    private ComboBox HouseNumberBox;
+    @FXML
+    private void initializeHNoBox(){
+        HouseNumberBox.setItems(HouseNumber);
+    }
+    
+    @FXML
+    private ComboBox MonthBox;
+    @FXML
+    private void initializeMonthBox(){
+        MonthBox.setItems(MonthPaid);
+    }
+    
+    @FXML
+    private TextField Amount;
+    @FXML
+    private void getAmount(){
+        Amount.getText();
+    }
+    
+    @FXML
+    private TextField Name;
+    @FXML
+    private void getName(){
+        Name.getText();
+    }
+    @FXML
+    private void createTable(){
+        String url = "jdbc:sqlite:C:\\Users\\Mike\\Documents\\NetBeansProjects\\SQLite\\ResidentialRentalManagementSoftware.sqlite";
+        String sql = "CREATE TABLE IF NOT EXISTS JatomApts (\n"
+                +"HouseNumber text PRIMARY KEY, \n"
+                +"Amount text NOT NULL, \n"
+                +"PayerName text NOT NULL, \n"
+                +"Month text NOT NULL\n"
+                +");";
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()){
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }        
+    private void insert(String HouseNumber, String Amount, String PayerName, String Month){
+        String url = "jdbc:sqlite:C:\\Users\\Mike\\Documents\\NetBeansProjects\\SQLite\\ResidentialRentalManagementSoftware.sqlite";
+        String sql1 = "INSERT INTO JatomApts(HouseNumber, Amount, PayerName, Month) VALUES(?,?,?,?)";
+        try (Connection conn = DriverManager.getConnection(url);
+                PreparedStatement pstmt = conn.prepareStatement(sql1)){
+            pstmt.setString(1, HouseNumber);
+            pstmt.setString(2, Amount);
+            pstmt.setString(3, PayerName);
+            pstmt.setString(4, Month);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private Button createTable;
+    
+    @FXML
+    private Button Submit;
+    @FXML
+    private void TableInsertButton(){
+        this.insert((String)HouseNumberBox.getSelectionModel().getSelectedItem(), Amount.getText(), Name.getText(), (String)MonthBox.getSelectionModel().getSelectedItem());
+    }
+        
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
